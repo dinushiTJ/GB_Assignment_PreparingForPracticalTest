53d2587976cc73b31c0dc008a5ec5fa774cdd6aa
dinushiTJ <dinushitj@gmail.com>
2021-05-14 13:29:17 +0530

    Created doPut() method.

--- PaymentService_AssignmentPreparation/src/main/java/com/paymentclientapp/dcbus/PaymentAPI.java
+++ PaymentService_AssignmentPreparation/src/main/java/com/paymentclientapp/dcbus/PaymentAPI.java
@@ -1,7 +1,10 @@
 package com.paymentclientapp.dcbus;
 
 import java.io.IOException;
-
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -79,6 +82,50 @@ public class PaymentAPI extends HttpServlet {
 		}
 		response.getWriter().append(responseJson.toString());
 	}
+	
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JsonObject responseJson = null;
+
+		try {
+			Map<String, String> parameterMap = getParameterMap(request);
+			String payment_id = parameterMap.get("paymentid").toString();
+			String consumer_id = parameterMap.get("consid").toString();
+			String product_id = parameterMap.get("prodid").toString();
+			float payment_amount = Float.parseFloat(parameterMap.get("payamount").toString());
+			String creditcard_no = parameterMap.get("ccnumber").toString();
+			String card_type = parameterMap.get("cctype").toString();
+			
+			//test parameters
+			System.out.println("PARAMS: pay="+payment_id+" cid="+consumer_id +" pid="+ product_id +" amt="+ payment_amount+" cc="+creditcard_no+" cct="+card_type);
+			
+			JsonObject updateResponseJson = paymentObj.updatePayment(payment_id, consumer_id, product_id, payment_amount, creditcard_no, card_type);
+
+			if (! updateResponseJson.get("STATUS").getAsString().equalsIgnoreCase("SUCCESSFUL")) {
+				responseJson = new JsonObject();
+				responseJson.addProperty("status", "error");
+				responseJson.addProperty("data", "Error occurred while updating the payment with payment id: " + payment_id);
+				response.getWriter().append(responseJson.toString());
+				return;
+			}
+
+			JsonObject tableJson = generatePaymentTable(paymentObj.readPayments(null));
+			responseJson = new JsonObject();
+			responseJson.addProperty("status", "success");
+			responseJson.addProperty("data", tableJson.get("payments").getAsString());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			responseJson = new JsonObject();
+			responseJson.addProperty("status", "error");
+			responseJson.addProperty("data", "Exception occurred while updating the payment.");
+		}
+		response.getWriter().append(responseJson.toString());
+	}
+
+
 
 	private JsonObject generatePaymentTable(JsonObject paymentsJson) {
 		//generating payments table
@@ -128,6 +175,32 @@ public class PaymentAPI extends HttpServlet {
 
 		return responseJson;
 	}
+	
+	// Convert request parameters to a Map
+		private static Map<String,String> getParameterMap(HttpServletRequest request)
+		{
+			Map<String, String> map = new HashMap<String, String>();
+			try
+			{
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				String queryString = scanner.hasNext() ?
+						scanner.useDelimiter("\\A").next() : "";
+				scanner.close();
+				String[] params = queryString.split("&");
+				for (String param : params)
+				{
+					String[] p = param.split("=");
+
+					//decoding the string before putting into the map to avoid undesired strings
+					map.put(p[0], java.net.URLDecoder.decode(p[1], StandardCharsets.UTF_8.name()));
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			return map;
+		}
 
 
 }
